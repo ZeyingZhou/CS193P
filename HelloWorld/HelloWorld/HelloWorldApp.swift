e6186fedcfb34348103d448a910fac4df0782c6a
Zeying Zhou <zeyingzhou@mac.home>
2025-11-29 12:38:49 -0500

    HelloWolrd Project

---
+++ HelloWorld/HelloWorld/HelloWorldApp.swift
@@ -0,0 +1,17 @@
+//
+//  HelloWorldApp.swift
+//  HelloWorld
+//
+//  Created by Zeying Zhou on 2025-11-29.
+//
+
+import SwiftUI
+
+@main
+struct HelloWorldApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
